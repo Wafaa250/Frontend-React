2b624f3c42ef9068223fbd77e15242e4fc70c3d8
Wafaa250 <wafaaabosamra78@email.com>
2026-03-12 13:15:53 +0200

    Add Counter component using useState

--- Frontend-React/src/App.jsx
+++ Frontend-React/src/App.jsx
@@ -4,17 +4,13 @@ import ItemList from "./components/ItemList"
 import StyledComponent from "./components/StyledBox"
 import StyledComponentCSS from "./components/StyledComponentCSS"
 import UserCard from "./components/UserCard"
-
+import Counter from "./components/Counter"
 
 function App() {
-
   return (
     <div>
 
-      <UserCard 
-        name="Wafaa"
-        major="Computer Engineering"
-      />
+      <Counter />
 
     </div>
   )
