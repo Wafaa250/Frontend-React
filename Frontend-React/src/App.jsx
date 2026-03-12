5722a95796630ad8bcb845e91ee862891a6b9ad7
Wafaa250 <wafaaabosamra78@email.com>
2026-03-12 11:19:42 +0200

    Initial React tasks (HelloWorld, ProfileCard, map list)

---
+++ Frontend-React/src/App.jsx
@@ -0,0 +1,12 @@
+import HelloWorld from "./HelloWorld"
+import ProfileCard from "./components/ProfileCard"
+import ItemList from "./ItemList"
+function App() {
+  return (
+    <div>
+      <ItemList />
+    </div>
+  )
+}
+
+export default App
