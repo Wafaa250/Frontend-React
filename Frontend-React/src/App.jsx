837046fa5b1d910767395169aaec5a5892916eaa
Wafaa250 <wafaaabosamra78@email.com>
2026-03-12 12:04:45 +0200

    Add  styling components

--- Frontend-React/src/App.jsx
+++ Frontend-React/src/App.jsx
@@ -1,10 +1,16 @@
 import HelloWorld from "./HelloWorld"
 import ProfileCard from "./components/ProfileCard"
 import ItemList from "./ItemList"
+import StyledComponent from "./StyledBox"
+import StyledComponentCSS from "./StyledComponentCSS"
+
+
 function App() {
   return (
     <div>
-      <ItemList />
+
+      <StyledComponentCSS />
+
     </div>
   )
 }
---
+++ Frontend-React/src/StyledComponentCSS.jsx
@@ -0,0 +1,16 @@
+function StyledComponentCSS() {
+
+  return (
+    <div className="card">
+
+      <h2 className="title">CSS Style Example</h2>
+
+      <p className="text">
+        This component is styled using CSS classes
+      </p>
+
+    </div>
+  )
+}
+
+export default StyledComponentCSS
